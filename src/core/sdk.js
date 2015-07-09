04f9af55d6ee8433e26aec86757cd82f4c21d051
Michal Czyzynski <michal.czyzynski@cherrygroup.com>
2015-07-09 13:41:54 +0200

    Initial setup

---
+++ src/core/sdk.js
@@ -0,0 +1,20 @@
+define(function () {
+    "use strict";
+
+    var sdk = {};
+
+    sdk.config = function (conf) {
+        console.log('config', conf);
+    };
+    sdk.pageView = function () {
+        console.log('page view');
+    };
+    sdk.track = function (eventName, properties) {
+        console.log('track', eventName, properties);
+    };
+    sdk.identify = function (uid, properties) {
+        console.log('identify', uid, properties);
+    };
+
+    return sdk;
+});
